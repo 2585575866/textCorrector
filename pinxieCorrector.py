295acd3bb44a9477424c9f1d9baa801b4f0a1f5d
2585575866@qq.com <2585575855@qq.com>
2019-05-10 16:49:30 +0800

    Initial commit
    Former-commit-id: 937a5dcccc5a7fbdc5910b165f29e477743248c9

---
+++ pinxieCorrector.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @Time    : 2019/5/7 14:40
+# @Author  : lxx
+# @File    : pinxieCorrector.py
+# @Software: PyCharm
+
+import pycorrector
+
+import jieba
+
+sen='他指出，这是他们计划在白俄罗斯访问的第一所大学，因此对此感到既激动又担忧，幸好一切都进行得很顺利。'
+corrected_sent, detail = pycorrector.correct(sen)
+print(sen)
+print(corrected_sent)
+
---
+++ yuyiCorrector.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @Time    : 2019/5/7 15:29
+# @Author  : lxx
+# @File    : yuyiCorrector.py
+# @Software: PyCharm
+import kenlm
+import jieba
+
+
+all_train=[]
+for line in open("data/data.train",encoding="utf-8"):
+    line=line.strip()
+    line=line.split("\t")
+    sentens=line[2:]
+    if len(sentens) >=2:
+        for i in range(1,len(sentens)):
+            tmp = []
+            tmp.append(sentens[0])
+            tmp.append(sentens[i])
+            all_train.append(tmp)
+    else:
+        all_train.append([sentens[0],sentens[0]])
+
+print(len(all_train))
+
+
+lm = kenlm.Model("kenlm-model/people_chars_lm.klm")
+err_count=0
+for pair_sen in all_train:
+    sen_seg0=jieba.cut(pair_sen[0])
+    sen_seg1=jieba.cut(pair_sen[1])
+
+    ppl_score0=lm.perplexity(' '.join(sen_seg0))
+    ppl_score1=lm.perplexity(' '.join(sen_seg1))
+    if ppl_score1 > ppl_score0:
+        err_count+=1
+print(err_count)
+print(len(all_train)-err_count)
